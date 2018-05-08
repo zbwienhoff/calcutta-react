218b6ce564456ba8aad2f4e76d0864c0683b3628
Chris Burke <burke1791@gmail.com>
2018-05-08 06:58:39 -0500

    small changes

---
+++ src/button.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="simple-button">
+        <form type="submit" value="Create"></form>
+      </div>
+    );
+  }
+}
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Button from './button';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Button />, document.getElementById('root'));
 registerServiceWorker();
